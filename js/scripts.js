84775ad118308926d8b36a366043d0720a6abe57
Dominik Magic <dominikmagic5@gmail.com>
2022-10-28 09:11:32 -0700

    third test for beepBoop fx passed

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@
 function beebBoop(input) {
   const userInput = Number(input);
     console.log('input ' + userInput);
-  if(!Number.isInteger(userInput)) {
-    console.log('pelase enter a whole number');
+  if(!Number.isInteger(userInput) || userInput < 1) {
+    console.log('please enter a number bigger than 0'); //needs to print out to DOM once completed
   }
 }
