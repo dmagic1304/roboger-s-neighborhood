372b1ce9b5221f31ca62b0d0519c139b48b79795
Dominik Magic <dominikmagic5@gmail.com>
2022-10-28 14:08:34 -0700

    It will show an error message if a whole number is not entered/created displayError and clearError fx in JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,12 @@
 // Bussiness logic
 function beebBoop(e) {
   e.preventDefault();
+  clearError();
   const userInput = Number(document.querySelector('input').value);
-  console.log('input value: ' + userInput);
   let outputArray = []; 
 
-  if(!Number.isInteger(userInput) || userInput < 1) {
-    console.log('Incorrect input'); //add UI function that will print to DOM
+  if(!Number.isInteger(userInput) || userInput < 1) {    
+    displayError();    
   } else {
     for(i = 0; i <= userInput; i++) {      
       outputArray.push(" " + String(i));
@@ -22,17 +22,26 @@ function beebBoop(e) {
       outputArray[index] = " Beep!";
     }
   });
-
   displayResult(outputArray);
 }
 
 
 // UI Logic
 
-function displayResult (result) {
+function displayResult(result) {
   const output = document.getElementById('result');
-  output.innerHTML = result;
+  output.innerText = result;
   output.removeAttribute('class');
+}
+
+function displayError() {
+  const output = document.getElementById('error');
+  output.removeAttribute('class');
+}
+
+function clearError() {
+  const output = document.getElementById('error');
+  output.setAttribute('class', 'hidden');
 }
 
 window.addEventListener('load', function() {
